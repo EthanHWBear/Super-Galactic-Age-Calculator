dbe2bd1b3bd6e15a883eca8c68868c7ba4c80cb5
Ethan Bear <ehwbear@icloud.com>
2021-11-11 14:50:15 -0800

    Rewrite test

--- __tests__/spaceAge.test.js
+++ __tests__/spaceAge.test.js
@@ -5,11 +5,10 @@ describe('Age Calculator', () => {
   test('should return user input with multiplied result for age on Mercury', () => {
     const planetAge = new Age();
     expect(age).mercury).toEqual(age*0.24);
-    function spaceAge (age) {
-      return parseFloat(age*2)
   }
   });
 
+  const age = $("#age").val();
 
 
   // test('should correctly determine whether three lengths are not a triangle', () => {
@@ -17,3 +16,6 @@ describe('Age Calculator', () => {
   //   expect(notTriangle.checkType()).toEqual("not a triangle");
   //});
 });
+
+// function spaceAge (age) {
+//   return parseFloat(age*2)
